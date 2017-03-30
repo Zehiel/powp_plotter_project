991804229f2fff672a65ed59cab7005bfd543069
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-03-31 01:57:47 +0200

    Changed driver name for simulator to more suitable :)

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -18,82 +18,76 @@ import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 
 
-public class TestPlotSoftPatterns
-{
-	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-		
+public class TestPlotSoftPatterns {
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     /**
-	 * Setup test concerning preset figures in context.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupPresetTests(Context context) {
-	    SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
-		
-		context.addTest("Figure Joe 1", selectTestFigureOptionListener);
-	}
+     * Setup test concerning preset figures in context.
+     *
+     * @param context Application context.
+     */
+    private static void setupPresetTests(Context context) {
+        SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
 
-	/**
-	 * Setup driver manager, and set default IPlotter for application.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupDrivers(Context context) {
-		IPlotter clientPlotter = new ClientPlotter();
-		context.addDriver("Client Plotter", clientPlotter);
-		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
-		
-		IPlotter plotter = new PlotterAdapter(ApplicationWithDrawer.getDrawPanelController());
-		context.addDriver("Buggy Simulator", plotter);
+        context.addTest("Figure Joe 1", selectTestFigureOptionListener);
+    }
 
-		context.updateDriverInfo();
-	}
+    /**
+     * Setup driver manager, and set default IPlotter for application.
+     *
+     * @param context Application context.
+     */
+    private static void setupDrivers(Context context) {
+        IPlotter clientPlotter = new ClientPlotter();
+        context.addDriver("Client Plotter", clientPlotter);
+        Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Context context) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
+        IPlotter plotter = new PlotterAdapter(ApplicationWithDrawer.getDrawPanelController());
+        context.addDriver("Bug free simulator", plotter);
+
+        context.updateDriverInfo();
+    }
+
+    /**
+     * Auxiliary routines to enable using Buggy Simulator.
+     *
+     * @param context Application context.
+     */
+    private static void setupDefaultDrawerVisibilityManagement(Context context) {
+        DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
         context.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
-        		new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
+                new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
         defaultDrawerWindow.setVisible(true);
-	}
-	
-	/**
-	 * Setup menu for adjusting logging settings.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupLogger(Context context) {
-		Application.addComponent(Logger.class);
-		context.addComponentMenu(Logger.class, "Logger", 0);
-		context.addComponentMenuElement(Logger.class, "Clear log", (ActionEvent e) -> context.flushLoggerOutput());
-		context.addComponentMenuElement(Logger.class, "Fine level", (ActionEvent e) -> LOGGER.setLevel(Level.FINE));
-		context.addComponentMenuElement(Logger.class, "Info level", (ActionEvent e) -> LOGGER.setLevel(Level.INFO));
-		context.addComponentMenuElement(Logger.class, "Warning level", (ActionEvent e) -> LOGGER.setLevel(Level.WARNING));
-		context.addComponentMenuElement(Logger.class, "Severe level", (ActionEvent e) -> LOGGER.setLevel(Level.SEVERE));
-		context.addComponentMenuElement(Logger.class, "OFF logging", (ActionEvent e) -> LOGGER.setLevel(Level.OFF));
-	}
-		
+    }
+
+    /**
+     * Setup menu for adjusting logging settings.
+     *
+     * @param context Application context.
+     */
+    private static void setupLogger(Context context) {
+        Application.addComponent(Logger.class);
+        context.addComponentMenu(Logger.class, "Logger", 0);
+        context.addComponentMenuElement(Logger.class, "Clear log", (ActionEvent e) -> context.flushLoggerOutput());
+        context.addComponentMenuElement(Logger.class, "Fine level", (ActionEvent e) -> LOGGER.setLevel(Level.FINE));
+        context.addComponentMenuElement(Logger.class, "Info level", (ActionEvent e) -> LOGGER.setLevel(Level.INFO));
+        context.addComponentMenuElement(Logger.class, "Warning level", (ActionEvent e) -> LOGGER.setLevel(Level.WARNING));
+        context.addComponentMenuElement(Logger.class, "Severe level", (ActionEvent e) -> LOGGER.setLevel(Level.SEVERE));
+        context.addComponentMenuElement(Logger.class, "OFF logging", (ActionEvent e) -> LOGGER.setLevel(Level.OFF));
+    }
+
     /**
      * Launch the application.
      */
-    public static void main(String[] args)
-    {
-        EventQueue.invokeLater(new Runnable()
-        {
-            public void run()
-            {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
                 ApplicationWithDrawer.configureApplication();
                 Context context = Application.getComponent(Context.class);
-                
-                //setupDefaultDrawerVisibilityManagement(context);
-                
-            	setupDrivers(context);
-            	setupPresetTests(context);
-            	setupLogger(context);
+
+                setupDrivers(context);
+                setupPresetTests(context);
+                setupLogger(context);
             }
 
         });
